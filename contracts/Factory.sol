acd6ec28a3eaeac9d936b43798fcba431035c7d6
Mario Frasca <mariotomo@gmail.com>
2023-03-30 09:31:24 +0200

    aggiustando il tiro

--- contracts/Factory.sol
+++ contracts/Factory.sol
@@ -2,7 +2,21 @@
 pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/proxy/Clones.sol";
+import "./Logic.sol";
 
 contract Factory {
-       
+    address immutable tokenImplementation;
+
+    constructor() payable {
+        tokenImplementation = address(new Logic());
+    }
+
+    receive() external payable {}
+
+    function createToken() external returns (address) {
+        address clone = Clones.clone(tokenImplementation);
+        Logic(payable(clone)).initialize(0, tokenImplementation);
+        return clone;
+    }
+    
 }
--- contracts/Logic.sol
+++ contracts/Logic.sol
@@ -1,13 +1,35 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
+import "@openzeppelin/contracts/proxy/Clones.sol";
 
 contract Logic {
     uint256 state;
-    constructor() payable {}
-    function raise() external {
-        if(state<5) state++;
+    uint256 age;
+    address mylogic;
+    address private owner;
+    constructor() payable {
+        owner = msg.sender;
+    }
+    receive() external payable {}
+    function initialize(uint256 _state, address _mylogic) public {
+        age = 0;
+        state = _state;
+        mylogic = _mylogic;
+    }
+    function raise() external returns(address) {
+        age++;
+        state++;
+        if(state>5) {
+            state = 3;
+            address result = address(Clones.clone(mylogic));
+            Logic(payable(result)).initialize(3, mylogic);
+            return(result);
+        } else {
+            return address(0x0);
+        }
     }
     function lower() external {
+        age++;
         if(state>0) state--;
     }
 }
